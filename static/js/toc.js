ce997dcfdf9b0c2976a435a15ec7a60464d3fd21
Shawn-Summer <362847502@qq.com>
2025-09-13 22:06:20 +0800

    enhance UI

--- static/js/toc.js
+++ static/js/toc.js
@@ -2,7 +2,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const toc = document.getElementById("post-toc");
   const toggle = document.getElementById("toc-toggle");
-
+  const tocContent = document.getElementById("TableOfContents"); // 核心内容容器
+  if(tocContent.textContent.trim() == ''){
+    toggle.style.display = "none";
+    toc.style.display = "none";
+  }
   toggle.addEventListener("click", function (e) {
     e.preventDefault();
     toc.classList.toggle("hidden");
